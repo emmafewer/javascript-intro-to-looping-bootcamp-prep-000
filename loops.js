75e4213ada740d38b3eaf3c36367a1fa3807eaa3
Emma Fewer <emmafewer20@gmail.com>
2020-09-15 17:40:33 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -12,7 +12,8 @@ function forLoop(array) {
 function whileLoop(number) {
   let countdown = n;
   
-    while (countdown > 0) {
+  while (countdown > 0) {
     console.log(--countdown)
   }
+  return "done"
 }
