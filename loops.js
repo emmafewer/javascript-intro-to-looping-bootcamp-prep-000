5b8c2233dc2defab3bb37ffbe28659ca824861a6
Emma Fewer <emmafewer20@gmail.com>
2020-09-15 17:45:34 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -18,6 +18,8 @@ function whileLoop(number) {
   return "done"
 }
 
-function doWhileLoop(integer) {
-  
+function doWhileLoop(integer){
+  do{
+    console.log("I run once regardless.")
+  } while (incrementVariable() <= integer)
 }
